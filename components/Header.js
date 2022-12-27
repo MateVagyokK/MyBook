e2195338c01257b5b52430b21013d50f4e96f03b
Saskó Máté <sasko.mate.03@gmail.com>
2022-12-27 18:28:49 +0100

    upgrades to nextauth v4

--- components/Header.js
+++ components/Header.js
@@ -14,6 +14,7 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+
 function Header() {
   return (
     <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
