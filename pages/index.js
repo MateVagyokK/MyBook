3eb89ab0465b928ed715443854eac1c51444abd5
Saskó Máté <sasko.mate.03@gmail.com>
2023-01-04 23:08:32 +0100

    add stories

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,23 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Login from "../components/Login";
 import Sidebar from "../components/Sidebar";
+import Feed from "../components/Feed";
 
 export default function Home({session}) {
   if(!session)return <Login />;
   return (
-    <div>
+    <div className="h-screen bg-gray-100 overflow-hidden">
       <Head>
         <title>MyBook</title>
       </Head>
 
       <Header />
 
-      <main>
+      <main className="flex">
         {/*Sidebar*/}
         <Sidebar />
         {/*Feed*/}
+        <Feed/>
         {/*Widgets*/}
       </main>
     </div>
